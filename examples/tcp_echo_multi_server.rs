d9136f925344af00a78dee2ff01332c3ee68db3b
pinkforest <36498018+pinkforest@users.noreply.github.com>
2022-06-10 02:12:36 +1000

    refactor

--- examples/tcp_echo_multi_server.rs
+++ examples/tcp_echo_multi_server.rs
@@ -5,8 +5,7 @@
 //! 3) To implement simple linebuffer echo back
 //! 4) With ability to listen on multiple local addresses
 //!
-use lunatic::{net, Mailbox};
-use lunatic::{sleep, spawn_link};
+use lunatic::{net, Mailbox, sleep, spawn_link};
 use std::io::{BufRead, BufReader, Write};
 use std::time::Duration;
 
@@ -16,22 +15,25 @@ struct TcpServer {
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-struct TcpClient {
+struct TcpPeer {
     tcp_stream: lunatic::net::TcpStream,
     peer: std::net::SocketAddr,
 }
 
 #[lunatic::main]
 fn main(_: Mailbox<()>) {
-    let listen1 = TcpServer {
-        local_address: "127.0.0.1:6666".to_owned(),
-    };
-    let listen2 = TcpServer {
-        local_address: "127.0.0.1:6667".to_owned(),
-    };
+    let local_addrs = vec![
+        TcpServer {
+            local_address: "127.0.0.1:6666".to_owned(),
+        },
+        TcpServer {
+            local_address: "127.0.0.1:6667".to_owned(),
+        },
+    ];
 
-    spawn_link!(|input = listen1 | listen(input));
-    spawn_link!(|input = listen2 | listen(input));
+    for l in local_addrs {
+        spawn_link!(|input = l| listen(input));
+    }
 
     loop {
         // Mainloop every 10s
@@ -39,7 +41,7 @@ fn main(_: Mailbox<()>) {
     }
 }
 
-// Creates a TcpServer Listener
+// Creates a TcpListen Listener
 fn listen(input: TcpServer) {
     let listener = net::TcpListener::bind(input.local_address.clone()).unwrap();
     println!("Listening on addr: {}", listener.local_addr().unwrap());
@@ -47,21 +49,21 @@ fn listen(input: TcpServer) {
     while let Ok((tcp_stream, peer)) = listener.accept() {
         println!("Accepted peer {} on addr: {}", peer, &input.local_address);
 
-        let tcp_client = TcpClient { tcp_stream, peer };
+        let tcp_peer = TcpPeer { tcp_stream, peer };
 
-        spawn_link!(|input = tcp_client | respond(input));
+        spawn_link!(|input = tcp_peer | respond(input));
     }
 }
 
 // Respond back to Line buffered input
-fn respond(mut client: TcpClient) {
-    let mut buf_reader = BufReader::new(client.tcp_stream.clone());
+fn respond(mut peer: TcpPeer) {
+    let mut buf_reader = BufReader::new(peer.tcp_stream.clone());
     loop {
         let mut buffer = String::new();
         let read = buf_reader.read_line(&mut buffer).unwrap();
         if buffer.contains("exit") || read == 0 {
             return;
         }
-        client.tcp_stream.write_all(buffer.as_bytes()).unwrap();
+        peer.tcp_stream.write_all(buffer.as_bytes()).unwrap();
     }
 }
