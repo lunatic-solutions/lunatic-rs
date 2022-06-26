bce97acec59129b6270fdf98708983156532f8aa
pinkforest <36498018+pinkforest@users.noreply.github.com>
2022-06-26 12:11:40 +1000

    fix/fmt to the fmt queen

--- tests/udp.rs
+++ tests/udp.rs
@@ -1,4 +1,4 @@
-use lunatic::{net};
+use lunatic::net;
 use lunatic_test::test;
 
 #[test]
@@ -6,10 +6,12 @@ fn udp_ping_connect_recv_send_main() {
     let sender = net::UdpSocket::bind("127.0.0.1:0").unwrap();
     let receiver = net::UdpSocket::bind("127.0.0.1:0").unwrap();
     let receiver_addr = receiver.local_addr().unwrap();
-    
+
     sender.connect(receiver_addr).expect("couldn't connect");
-    sender.send("P1NG".as_bytes()).expect("couldn't send message");
-    
+    sender
+        .send("P1NG".as_bytes())
+        .expect("couldn't send message");
+
     let mut buf = [0; 4];
     let len_in = receiver.recv(&mut buf).unwrap();
 
@@ -23,9 +25,11 @@ fn udp_ping_recv_from_send_to_main() {
     let sender_addr = sender.local_addr().unwrap();
     let receiver = net::UdpSocket::bind("127.0.0.1:0").unwrap();
     let receiver_addr = receiver.local_addr().unwrap();
-    
-    sender.send_to("P2NG".as_bytes(), receiver_addr).expect("couldn't send message");
-    
+
+    sender
+        .send_to("P2NG".as_bytes(), receiver_addr)
+        .expect("couldn't send message");
+
     let mut buf = [0; 4];
     let (len_in, addr_in) = receiver.recv_from(&mut buf).unwrap();
 
