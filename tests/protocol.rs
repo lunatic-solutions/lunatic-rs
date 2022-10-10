2e3eba636447114e824322f7c17d210b79a50d86
Kai Jellinghaus <contact@kaij.tech>
2022-10-10 17:24:16 +0200

    Recursive protocols (#79)
    * Add Rec<P>
    
    * Remove accidental change
    
    * Rework Protocol to use a compile-time stack
    
    * Remove comment
    
    Co-authored-by: Bernard Kolobara <bernard.kolobara@gmail.com>

--- tests/protocol.rs
+++ tests/protocol.rs
@@ -1,10 +1,12 @@
-use lunatic::protocol::{End, Protocol, Send};
-use lunatic::Process;
+use lunatic::{Process};
 use lunatic_test::test;
 
 #[test]
 #[should_panic]
 fn drop_unfinished() {
+    use lunatic::protocol::End;
+    use lunatic::protocol::Protocol;
+    use lunatic::protocol::Send;
     let protocol = Process::spawn_link((), |_, _: Protocol<Send<(), End>>| {
         // Protocol dropped without sending a message back.
     });
@@ -16,6 +18,9 @@ fn drop_unfinished() {
 fn msg_pack_serializer() {
     use lunatic::protocol::Recv;
     use lunatic::serializer::MessagePack;
+    use lunatic::protocol::End;
+    use lunatic::protocol::Protocol;
+    use lunatic::protocol::Send;
 
     let protocol = Process::spawn_link(
         (),
@@ -29,4 +34,46 @@ fn msg_pack_serializer() {
     let protocol = protocol.send(input);
     let (_, result) = protocol.receive();
     assert_eq!(0.88, result);
+}
+
+#[test]
+fn recursive_protocols() {
+    use lunatic::protocol::Rec;
+    use lunatic::protocol::Recv;
+    use lunatic::protocol::Send;
+    use lunatic::protocol::End;
+    use lunatic::protocol::Pop;
+    use lunatic::protocol::Offer;
+    use lunatic::protocol::Branch;
+    use lunatic::protocol::Protocol;
+    type P = Offer<Recv<u64, Send<u64, Pop>>, End>;
+
+    let protocol = Process::spawn_link(
+        (),
+        |(), proto: Protocol<Rec<P>>| {
+
+            let mut loop_protocol = proto.repeat();
+            loop {
+                match loop_protocol.offer() {
+                    Branch::Left(protocol) => {
+                        let (protocol, v) = protocol.receive();
+                        let protocol = protocol.send(v * 2);
+                        loop_protocol = protocol.pop().repeat();
+                    },
+                    Branch::Right(_end) => break,
+                };
+            }
+        }
+    );
+
+    let mut loop_protocol = protocol.repeat();
+    for i in 0..5 {
+        let protocol = loop_protocol.select_left();
+        let protocol = protocol.send(i);
+        let (end, value) = protocol.receive();
+        assert_eq!(i * 2, value);
+        loop_protocol = end.pop().repeat();
+    }
+
+    let _end = loop_protocol.select_right();
 }
