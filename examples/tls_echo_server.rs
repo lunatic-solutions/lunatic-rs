7b35d895c8ffb9b0c3bff520aaf4b48c0cae9e01
Yuriy Voshchepynets <71330860+SquattingSocrates@users.noreply.github.com>
2022-10-17 11:03:07 +0200

    Tls streams (#82)
    * implement getters/setters of tcp timeouts
    
    * add pr suggestions
    
    * expose call to tcp_peek and fix pr suggestions
    
    * remove flush timeout
    
    * add basic support for tls
    
    * minimal working version of tls client
    
    * cleanup tls stream logic
    
    * fix clippy error
    
    * fix clippy issue
    
    * fix open suggestions

---
+++ examples/tls_echo_server.rs
@@ -0,0 +1,36 @@
+use lunatic::{net, Mailbox, Process};
+use std::io::{BufReader, Read, Write};
+
+fn main() {
+    let key = std::fs::read("./examples/CA/localhost.key")
+        .expect("Should have been able to read the file");
+    let cert = std::fs::read("./examples/CA/localhost.crt").expect("Should have read cert file");
+    let listener = net::TlsListener::bind("127.0.0.1:3000", cert, key).unwrap();
+    println!("Listening on addr: {}", listener.local_addr().unwrap());
+    while let Ok((tls_stream, _peer)) = listener.accept() {
+        Process::spawn(tls_stream, handle);
+    }
+}
+
+fn handle(mut tls_stream: net::TlsStream, _: Mailbox<()>) {
+    println!("Spawn new handler");
+    let mut buf_reader = BufReader::new(tls_stream.clone());
+    let mut buffer = [0u8; 100];
+    // TODO: do something with read data
+    let _read = buf_reader.read(&mut buffer).expect("Should have read line");
+
+    tls_stream
+        .write(
+            [
+                "HTTP/1.1 200 OK\n",
+                "Date: Wed, 28 Sep 2022 09:45:07 GMT",
+                "Content-Length: 12\n",
+                "Content-Type: text/html\n",
+                "\n\n",
+                "<h1>Hello world!</h1>",
+            ]
+            .concat()
+            .as_bytes(),
+        )
+        .unwrap();
+}
