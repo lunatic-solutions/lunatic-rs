9da61c9e1830bef1d53604d5149d5d1fab650f01
Marin Koštić <kosticmarin@gmail.com>
2022-11-11 15:59:13 +0100

    Feature/lookup nodes (#77)
    * Add lookup nodes host function
    
    - adds ability to filter nodes based
      on `key=value` node metadata
    
    * Add documentation
    
    * Add error handling for lookup nodes

--- src/distributed.rs
+++ src/distributed.rs
@@ -1,4 +1,6 @@
-use crate::host::api::distributed::{get_nodes, module_id, nodes_count};
+use crate::host::api::distributed::{
+    copy_lookup_nodes_results, exec_lookup_nodes, get_nodes, module_id, nodes_count,
+};
 use crate::host::api::{self};
 use crate::module::{params_to_vec, Param};
 use crate::LunaticError;
@@ -13,6 +15,41 @@ pub fn nodes() -> Vec<u64> {
     let copied_cnt = unsafe { get_nodes(nodes.as_mut_ptr(), cnt as u32) as usize };
     nodes.truncate(copied_cnt);
     nodes
+}
+
+/// Executes a lookup query request to the control node and returns `u64` node ids.
+///
+/// Query is defined like an URL Query string, e.g. `name=node01&group=workers`.
+pub fn lookup_nodes(query: &'static str) -> Result<Vec<u64>, LunaticError> {
+    let mut query_id = 0;
+    let mut nodes_len = 0;
+    let mut error_id = 0;
+    let result = unsafe {
+        exec_lookup_nodes(
+            query.as_ptr(),
+            query.len() as u32,
+            &mut query_id,
+            &mut nodes_len,
+            &mut error_id,
+        )
+    };
+    if result == 1 {
+        return Err(LunaticError::from(error_id));
+    }
+    let mut nodes = vec![0; nodes_len as usize];
+    let copied_cnt = unsafe {
+        copy_lookup_nodes_results(
+            query_id,
+            nodes.as_mut_ptr(),
+            nodes_len as u32,
+            &mut error_id,
+        )
+    };
+    if copied_cnt < 0 {
+        return Err(LunaticError::from(error_id));
+    }
+    nodes.truncate(copied_cnt as usize);
+    Ok(nodes)
 }
 
 pub fn spawn(node_id: u64, config_id: i64, entry: fn(i32), arg: i32) -> Result<u64, LunaticError> {
