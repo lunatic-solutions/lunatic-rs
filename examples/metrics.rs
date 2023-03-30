25d6d8b432a49609791d150a1ccab0440e9e5450
Ari Seyhun <ariseyhun@live.com.au>
2023-03-30 13:05:00 +1030

    feat: metrics macros

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -1,27 +1,114 @@
 use std::collections::HashMap;
+use std::fmt;
 // Lunatic with metrics enabled is required (enabled by default)
 // To collect the metrics, prometheus feature should be also enabled
 // and lunatic has to be run with --prometheus flag
 use std::time::Duration;
 
+use log::Level;
 use lunatic::metrics::{
     add_event, counter, decrement_gauge, histogram, increment_counter, increment_gauge, Span,
 };
-use lunatic::{sleep, Mailbox};
+use lunatic::{info, info_span, sleep, Mailbox};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} is {}", self.name, self.age)
+    }
+}
 
 #[lunatic::main]
 fn main(_: Mailbox<()>) {
-    let span = Span::new("myspan", &()).unwrap();
-    let mut attributes = HashMap::new();
-    attributes.insert("target", "MyModule");
-    attributes.insert("message", "Hello world!");
-    span.add_event("my_event", &attributes).unwrap();
+    // let span = info_span!("my_span");
 
-    let inner_span = Span::new_with_parent(&span, "my_inner_span", &()).unwrap();
-    inner_span.add_event("my_inner_event", &()).unwrap();
+    let null = ();
+    let bool_t = true;
+    let bool_f = false;
+    let number = 10.43;
+    let string = "Hello, World!";
+    let array = vec![1, 2, 3];
+    let object = Person {
+        name: "John Doe".to_string(),
+        age: 23,
+    };
+    // {
+    //     (|message: std::fmt::Arguments<'_>| {
+    //         let name = "event_:0";
+    //         let attrs = lunatic::metrics::Attributes::new(
+    //             ("module::path"),
+    //             (lunatic::metrics::Level::Info),
+    //             message,
+    //             "",
+    //             0,
+    //             0,
+    //             "module::path",
+    //             [].into_iter()
+    //                 .collect::<std::collections::BTreeMap<&'static str, serde_json::Value>>(),
+    //         );
+    //         lunatic::metrics::add_event(None, name, Some(&attrs));
+    //     })(format_args!("Hello {}!", bool_f))
+    // }
 
-    let inner_span_b = Span::new_with_parent(&span, "my_inner_span_b", &()).unwrap();
-    inner_span_b.add_event("my_inner_event_b", &()).unwrap();
+    info!(
+        null,
+        bool_t,
+        bool_f,
+        number,
+        string,
+        array,
+        object,
+        "Additional log message, with {}!",
+        "formatting"
+    );
+
+    // {
+    //     let name = "event_:0";
+    //     let attributes = {
+    //         let (message, attributes) = {
+    //             let mut message: Option<std::fmt::Arguments<'_>> = None;
+    //             let attributes: std::collections::BTreeMap<&'static str, serde_json::Value> = {
+    //                 message = Some(format_args!("Hello, world"));
+    //                 [].into_iter().collect()
+    //             };
+    //             (message, attributes)
+    //         };
+    //         lunatic::metrics::Attributes::new(
+    //             ("module::path"),
+    //             (lunatic::metrics::Level::Info),
+    //             message,
+    //             "",
+    //             0,
+    //             "module::path",
+    //             attributes,
+    //         )
+    //     };
+    //     lunatic::metrics::add_event(None, name, &attributes);
+    // }
+
+    // let span = Span::new("myspan", &()).unwrap();
+    // let mut attributes = HashMap::new();
+    // attributes.insert("target", "MyModule");
+    // attributes.insert("message", "Hello world!");
+    // span.add_event("my_event", &attributes).unwrap();
+    //
+    // let inner_span = Span::new_with_parent(&span, "my_inner_span", &()).unwrap();
+    // inner_span.add_event("my_inner_event", &()).unwrap();
+    //
+    // let inner_span_b = Span::new_with_parent(&span, "my_inner_span_b", &()).unwrap();
+    // inner_span_b.add_event("my_inner_event_b", &()).unwrap();
+    //
+    // let attributes = lunatic::valueset!(entry: "foo" = 1, "hey");
+    // dbg!(attributes);
+    //
+    // let attrs = lunatic::attributes!(target: "trgt", Level::Info, "foo" = 1, "hey");
+    // dbg!(attrs);
 
     // let span_id = unsafe {
     //     let name = "my_span";
--- src/metrics.rs
+++ src/metrics.rs
@@ -5,17 +5,26 @@
 //!
 //! All this functions are similar to the macros defined in [metrics docs](https://docs.rs/metrics/latest/metrics/index.html#emission)
 
-use serde::Serialize;
+use std::{
+    collections::BTreeMap,
+    fmt,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+pub use log::Level;
+use serde::{ser::SerializeMap, Serialize};
 
 use crate::host::{self, api::metrics};
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+mod macros;
+
+#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Span {
     id: u64,
 }
 
 impl Span {
-    pub fn new<T>(name: &str, attributes: &T) -> Result<Self, serde_json::Error>
+    pub fn new<T>(name: &str, attributes: Option<&T>) -> Result<Self, serde_json::Error>
     where
         T: Serialize + 'static,
     {
@@ -25,7 +34,7 @@ impl Span {
     pub fn new_with_parent<T>(
         parent: &Span,
         name: &str,
-        attributes: &T,
+        attributes: Option<&T>,
     ) -> Result<Self, serde_json::Error>
     where
         T: Serialize + 'static,
@@ -33,32 +42,27 @@ impl Span {
         Self::new_(Some(parent), name, attributes)
     }
 
-    fn new_<T>(parent: Option<&Span>, name: &str, attributes: &T) -> Result<Self, serde_json::Error>
+    fn new_<T>(
+        parent: Option<&Span>,
+        name: &str,
+        attributes: Option<&T>,
+    ) -> Result<Self, serde_json::Error>
     where
         T: Serialize + 'static,
     {
         let parent_id = parent.map(|span| span.id).unwrap_or(u64::MAX);
-        let id = if std::any::TypeId::of::<T>() == std::any::TypeId::of::<()>() {
-            unsafe {
-                host::api::metrics::start_span(
-                    parent_id,
-                    name.as_ptr(),
-                    name.len(),
-                    0 as *const u8,
-                    0,
-                )
-            }
-        } else {
-            let attributes_bytes = serde_json::to_vec(attributes)?;
-            unsafe {
-                host::api::metrics::start_span(
-                    parent_id,
-                    name.as_ptr(),
-                    name.len(),
-                    attributes_bytes.as_ptr(),
-                    attributes_bytes.len(),
-                )
-            }
+        let attributes_bytes = match attributes {
+            Some(attributes) => serde_json::to_vec(attributes)?,
+            None => vec![],
+        };
+        let id = unsafe {
+            host::api::metrics::start_span(
+                parent_id,
+                name.as_ptr(),
+                name.len(),
+                attributes_bytes.as_ptr(),
+                attributes_bytes.len(),
+            )
         };
 
         let span = Span { id };
@@ -66,7 +70,7 @@ impl Span {
         Ok(span)
     }
 
-    pub fn add_event<T>(&self, name: &str, attributes: &T) -> Result<(), serde_json::Error>
+    pub fn add_event<T>(&self, name: &str, attributes: Option<&T>) -> Result<(), serde_json::Error>
     where
         T: Serialize + 'static,
     {
@@ -82,31 +86,26 @@ impl Drop for Span {
     }
 }
 
-pub fn add_event<T>(span: Option<u64>, name: &str, attributes: &T) -> Result<(), serde_json::Error>
+pub fn add_event<T>(
+    span: Option<u64>,
+    name: &str,
+    attributes: Option<&T>,
+) -> Result<(), serde_json::Error>
 where
-    T: Serialize + 'static,
+    T: Serialize,
 {
-    if std::any::TypeId::of::<T>() == std::any::TypeId::of::<()>() {
-        unsafe {
-            host::api::metrics::add_event(
-                span.unwrap_or(u64::MAX),
-                name.as_ptr(),
-                name.len(),
-                0 as *const u8,
-                0,
-            )
-        }
-    } else {
-        let attributes_bytes = serde_json::to_vec(attributes)?;
-        unsafe {
-            host::api::metrics::add_event(
-                span.unwrap_or(u64::MAX),
-                name.as_ptr(),
-                name.len(),
-                attributes_bytes.as_ptr(),
-                attributes_bytes.len(),
-            )
-        }
+    let attributes_bytes = match attributes {
+        Some(attributes) => serde_json::to_vec(attributes)?,
+        None => vec![],
+    };
+    unsafe {
+        host::api::metrics::add_event(
+            span.unwrap_or(u64::MAX),
+            name.as_ptr(),
+            name.len(),
+            attributes_bytes.as_ptr(),
+            attributes_bytes.len(),
+        )
     }
 
     Ok(())
@@ -140,4 +139,82 @@ pub fn decrement_gauge(name: &str, value: f64) {
 /// Sets a histogram
 pub fn histogram(name: &str, value: f64) {
     unsafe { metrics::histogram(name.as_ptr(), name.len(), value) }
+}
+
+#[derive(Clone, Debug)]
+pub struct Attributes<'a> {
+    target: &'static str,
+    level: Level,
+    message: fmt::Arguments<'a>,
+    file: &'static str,
+    line: u32,
+    column: u32,
+    module_path: &'static str,
+    attributes: BTreeMap<&'static str, serde_json::Value>,
+    timestamp: SystemTime,
+}
+
+impl<'a> Attributes<'a> {
+    #[inline]
+    pub fn new(
+        target: &'static str,
+        level: Level,
+        message: fmt::Arguments<'a>,
+        file: &'static str,
+        line: u32,
+        column: u32,
+        module_path: &'static str,
+        attributes: BTreeMap<&'static str, serde_json::Value>,
+    ) -> Self {
+        Attributes {
+            target,
+            level,
+            message,
+            file,
+            line,
+            column,
+            module_path,
+            attributes,
+            timestamp: SystemTime::now(),
+        }
+    }
+}
+
+impl<'a> Serialize for Attributes<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let (severity_number, severity_text) = level_severity(&self.level);
+        let timestamp = self
+            .timestamp
+            .duration_since(UNIX_EPOCH)
+            .map_err(|_| serde::ser::Error::custom("Time went backwards"))?;
+
+        let message_is_empty = self.message.as_str().map(|s| s.is_empty()).unwrap_or(false);
+        let mut map = serializer.serialize_map(Some(9 + message_is_empty as usize))?;
+        if !message_is_empty {
+            map.serialize_entry("body", &self.message)?;
+        }
+        map.serialize_entry("target", &self.target)?;
+        map.serialize_entry("severityNumber", &severity_number)?;
+        map.serialize_entry("severityText", &severity_text)?;
+        map.serialize_entry("code.filepath", &self.file)?;
+        map.serialize_entry("code.lineno", &self.line)?;
+        map.serialize_entry("code.column", &self.column)?;
+        map.serialize_entry("code.namespace", &self.module_path)?;
+        map.serialize_entry("attributes", &self.attributes)?;
+        map.serialize_entry("timestamp", &timestamp)?;
+        map.end()
+    }
+}
+
+fn level_severity(level: &Level) -> (u8, &'static str) {
+    match level {
+        Level::Error => (17, "ERROR"),
+        Level::Warn => (13, "WARN"),
+        Level::Info => (9, "INFO"),
+        Level::Debug => (5, "DEBUG"),
+        Level::Trace => (1, "TRACE"),
+    }
 }
